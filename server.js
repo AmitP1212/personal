a7c878c42ced24466d5b916f44450fc91122971a
1212_AMIT <amitpisal1212@yahoo.com>
2022-06-20 20:03:48 +0530

    Changed for Personal Use

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ var express =require("express");  //lib-----Node Module
 var app=express();  // will create app object from express package
 
 var onAboutUs=function(req, res){
-    res.send("Chief Mentor :Ravi Tambade");
+    res.send("Chief Mentor :Amit Pisal");
 };
 var onDefault=function(req, res){
-    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
+    res.send("<h1><div style="color: rgb(252,0,0);">Dear Mummy and Maushi</div></h1>"+
               "<hr/>"+
-              "<ol>"+
-                    " <li>Mentoring for skill building</li>"+
-                    " <li>Workshops</li>"+
-                    " <li>Online Course</li>"+
-                    " <li>Corporate Training</li>"+
-             "</ol>");
+              "<ul>"+
+                    " <li><div style="color: rgb(107, 52, 211);">Mummy and Maushi, throughout the life, your valuable advices have led me safely. <br />Your words are my guidance, and your love is the most precious item to me. <br />Now, this is the opportunity to thank you for everything U did for me.</div></li>"+
+                    " <li><div style="color: rgb(214, 47, 172);">To the woman that always brought the best in me <br />or at least saw the best in me regardless of what I did.</div></li>"+
+                    " <li><div style="color: rgb(0, 91, 173);">My amazing mother and Maushi,<br/>Your birthday deserves all the amazing colorsof celebration. <br />You both are the best mother's in all the land </div></li>"+
+                    " <li><br /><h2><div style="color: rgb(174, 0, 255);">Happy Birthday Dear Lovely Ladies</div><h2></li>"+
+             "</ul>");
 };
 app.get("/",onDefault);  // Request handler functions are registered
 app.get("/aboutus",onAboutUs);  // Request handler functions are registered
